1d29664a9b1ca54ebd0801d518c7b26566168501
Carlo Martin <carlo.cool13@gmail.com>
2019-10-10 10:53:13 -0500

    feat(*): Added Login and NAvigation components structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,15 @@ import {
   MatIconModule,
   MatButtonModule,
   MatDividerModule,
-  MatExpansionModule
+  MatExpansionModule, MatToolbarModule, MatSidenavModule, MatListModule
 } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './users/login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   imports:
@@ -37,9 +40,13 @@ import { HttpClientModule } from '@angular/common/http';
       MatDividerModule,
       MatExpansionModule,
       BrowserAnimationsModule,
-      HttpClientModule
+      HttpClientModule,
+      LayoutModule,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatListModule
    ],
-  declarations: [ AppComponent, CardsComponent ],
+  declarations: [ AppComponent, CardsComponent, LoginComponent, NavigationComponent ],
   bootstrap:    [ AppComponent ]
 })
 
